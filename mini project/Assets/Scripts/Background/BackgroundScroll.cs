25b49609199b88b374bf234ffd5f0de277e0d234
JUAN KIM <anzuroid@kookmin.ac.kr>
2023-02-16 21:37:49 +0900

    0216
    -날아다니는 몬스터 추가했습니다
    
    -몬스터 피격 애니메이션 만들었습니다 이제 다른 몬스터 만들 때 EnemyControl 가져와서 만들면 됩니다
    
    -게임 오브젝트 이미지가 작아서 메인카메라 사이즈를 줄였습니다
    
    -풀 받았을때 화면 스크롤 안되고 장애물 안나오는 버그 고쳤습니다
    
    -점프 반복 시 간헐적으로 바닥에 붙는 버그 고쳤습니다

--- mini project/Assets/Scripts/Background/BackgroundScroll.cs
+++ mini project/Assets/Scripts/Background/BackgroundScroll.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Scroll : MonoBehaviour
+public class BackgroundScroll : MonoBehaviour
 {
     GameManager gameManager;
     public Transform[] backgrounds;
